a51f6b2cd8fbc11f4ed1bce156c96f3f8aab2d95
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-03-07 21:51:47 -0500

    Create skills component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { TopNavigation } from "./pages/Navigation/Top-navigation";
 import { Hero } from "./pages/Home/Hero/Hero";
 import { About } from "./pages/Home/About/About";
+import { Tools } from "./pages/Home/Tool-section/Tools-section";
 
 function App() {
     return (
         <>
             <TopNavigation />
             <Hero />
-            <About />
+            {/* <About /> */}
+            <Tools />
         </>
     );
 }
---
+++ src/pages/Home/Tool-section/Tools-section.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { PageContainer } from "../../components";
+import { skills } from "./data";
+
+const ToolWrapper = styled(PageContainer)`
+    width: 117.6rem;
+    margin-top: 20.7rem;
+    gap: var(--space-60);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const SkillCard = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 1.5rem;
+`;
+
+const SkillImage = styled.img`
+    width: 3rem;
+    height: 3rem;
+    margin-right: 1rem;
+`;
+
+const SkillTitle = styled.span`
+    font-size: var(--space-16);
+`;
+
+const Tools = () => {
+    return (
+        <ToolWrapper>
+            {skills.map((skill, index) => (
+                <SkillCard key={index}>
+                    <SkillImage src={skill.image} />
+                    <SkillTitle>{skill.title}</SkillTitle>
+                </SkillCard>
+            ))}
+        </ToolWrapper>
+    );
+};
+
+export { Tools };
