9afe9776fa32412413be9c1e33c9e00500c0a006
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-03-09 21:14:49 -0500

    Partially finish the aboutpart

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { TopNavigation } from "./pages/Navigation/Top-navigation";
 import { Hero } from "./pages/Home/Hero/Hero";
-import { About } from "./pages/Home/About/About";
 import { Tools } from "./pages/Home/Tool-section/Tools-section";
 import { Service } from "./pages/Home/Service/Service";
 import { Icon } from "./design-system";
+import { About } from "./pages/Home/About/About";
 
 function App() {
     return (
         <>
             <TopNavigation />
             <Hero />
-            {/* <About /> */}
             <Tools />
             <Service />
             <Icon />
+            <About />
         </>
     );
 }
--- src/pages/Home/About/About.tsx
+++ src/pages/Home/About/About.tsx
@@ -1,53 +1,121 @@
+import { Button, Icon, Input, Typography } from "../../../design-system";
 import styled from "styled-components";
-import { Button, Typography } from "../../../design-system";
-import { AboutCard } from "./AboutCard";
-import { aboutInfo } from "./data";
+import { PageContainer } from "../../components";
+import whiteLine from "../../../design-system/assets/image/line-decor-white.png";
+import ellipse from "../../../design-system/assets/image/ELLIPSE.png";
+import ellipseYellow from "../../../design-system/assets/image/ELLIPSE-yellow.png";
 
-const SectionBase = styled.section`
-    max-width: 192rem;
-    padding: 2rem 15.2rem;
+const AboutSection = styled(PageContainer)`
+    padding-left: 10rem;
+    padding-right: 6rem;
+    align-items: center;
+    display: flex;
+
+    background-color: var(--black-200);
 `;
 
-const FeaturesTextWrapper = styled.div`
-    text-align: center;
-    margin-bottom: var(--space-50);
+const AboutLeftWrapper = styled.div`
+    width: 60rem;
+    height: 59rem;
+    border-radius: var(--space-8);
+    background-color: var(--black-100);
 `;
 
-const Title = styled(Typography)`
-    color: var(--jaguar-900);
-    font-size: var(--space-40);
+const AboutSectionLeft = styled.div`
+    max-width: 64.5rem;
 `;
 
-// const Description = styled(Typography)`
-//     color: var(--jaguar-500);
-//     font-size: var(--space-30);
-// `;
+const AboutSectionRight = styled.div`
+    width: 63rem;
+    height: 59rem;
+    border-radius: var(--space-8);
 
-const AboutCardsWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: var(--space-30);
-    margin-bottom: var(--space-50);
+    margin-left: 3.5rem;
+    background-color: #656868;
+`;
+const AboutTitle = styled(Typography)`
+    padding-left: 10.7rem;
+    /* padding-top: 16rem; */
+
+    color: var(--white);
+    padding-bottom: var(--space-20);
+    padding-right: 17.9rem;
+`;
+const AboutDescription = styled(Typography)`
+    padding-left: 10.8rem;
+    padding-right: 10.7rem;
+    /* font-size: 5rem;
+    line-height: 5.5rem; */
+    display: block;
+    align-items: center;
+    text-align: left;
 `;
 
+const AboutParagraph = styled(Typography)`
+    color: var(--black-400);
+    padding-left: 10.8rem;
+    padding-right: 10.7rem;
+    padding-bottom: 3.8rem;
+    padding-top: 3rem;
+    line-height: 2.5rem;
+`;
+
+const WhiteLine = styled.div`
+    padding-left: 10.8rem;
+    padding-top: 3.8rem;
+    padding-bottom: 3.7rem;
+`;
+
+const AboutEllipseBlueWrapper = styled.div`
+    padding-top: 12.1rem;
+    padding-left: 48rem;
+`;
+
+const AboutYellowEllipse = styled.div`
+    padding-left: 9.1rem;
+    padding-bottom: 4rem;
+`;
+//
 const About = () => {
     return (
-        <SectionBase>
-            <FeaturesTextWrapper>
-                <Title variant="displayLG" weight="bold">
-                    About
-                </Title>
-                {/* <Description variant="displaySM" weight="medium">
-                    Our solution provides simple yet effective project
-                    management
-                </Description> */}
-            </FeaturesTextWrapper>
-            <AboutCardsWrapper>
-                {aboutInfo.map((feature, index) => (
-                    <AboutCard key={index} info={feature} />
-                ))}
-            </AboutCardsWrapper>
-        </SectionBase>
+        <AboutSection>
+            <AboutLeftWrapper>
+                <AboutSectionLeft>
+                    <AboutEllipseBlueWrapper>
+                        <img src={ellipse} alt="ellipse" />
+                    </AboutEllipseBlueWrapper>
+                    <AboutTitle
+                        weight="bold"
+                        variant="paragraphSM"
+                        align="inherit"
+                    >
+                        About Me
+                    </AboutTitle>
+                    <AboutDescription
+                        weight="normal"
+                        variant="paragraphMD"
+                        align="left"
+                    >
+                        Aboutd software engeneer located in Virginia.
+                    </AboutDescription>
+                    <AboutParagraph weight="semibold" variant="h4" align="left">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consectetur porro animi quidem at, veritatis quisquam
+                        fugit deleniti doloremque id illo quam magni dolorum
+                        amet nobis, quaerat eligendi rerum dolorem!
+                    </AboutParagraph>
+
+                    <WhiteLine>
+                        <img src={whiteLine} alt="line" />
+                    </WhiteLine>
+                </AboutSectionLeft>
+                <AboutYellowEllipse>
+                    <img src={ellipseYellow} alt="yellow" />
+                </AboutYellowEllipse>
+            </AboutLeftWrapper>
+
+            <AboutSectionRight></AboutSectionRight>
+        </AboutSection>
     );
 };
 
