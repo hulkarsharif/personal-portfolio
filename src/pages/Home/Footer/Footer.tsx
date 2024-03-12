02e218a290b684e12d6a3a019d890070b91a76e5
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-03-12 17:27:10 -0400

    partially done with input

--- src/pages/Home/Footer/Footer.tsx
+++ src/pages/Home/Footer/Footer.tsx
@@ -6,26 +6,51 @@ import orange from "../../../design-system/assets/image/HIGHLIGHT.png";
 const FooterSection = styled(PageContainer)`
     display: block;
     align-items: center;
-
     border-radius: var(--space-8);
 `;
 
 const FooterWrapper = styled.div`
     /* width: 100%; */
     height: 42.7rem;
-    padding-top: 8.6rem;
-    padding-bottom: 8.5rem;
-    padding-left: 12.3rem;
-    padding-right: 33.8rem;
+
     border-radius: var(--space-8);
-    background-color: var(--green-400);
+    background-color: var(--black-200);
 `;
 
 const FooterTitle = styled(Typography)`
+    padding-top: 3rem;
     justify-content: space-between;
-    padding-bottom: 4rem;
+    text-align: center;
+    padding-bottom: 3rem;
+`;
+const ContactForm = styled.form`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    padding: 5rem;
+    padding-right: 3.5rem;
+    padding-left: 3.5rem;
+
+    background-color: var(--black-100);
+    border-radius: var(--border-radius-10);
+    /* padding-bottom: 8rem; */
+    gap: 6rem;
+
+    > *:not(:last-child) {
+        margin-bottom: var(--space-20);
+    }
 `;
 
+const StyledTextarea = styled(Input)`
+    grid-column: span 2;
+    width: 100%;
+    /* height: 15rem; */
+
+    textarea {
+        resize: none;
+        height: 4rem;
+        overflow: hidden;
+    }
+`;
 const SocialMedia = styled.div`
     color: var(--black-900);
     gap: 3.6rem;
@@ -50,21 +75,63 @@ const Footer = () => {
     return (
         <FooterSection>
             <FooterWrapper>
-                <FooterTitle weight="bold" variant="h3" align="inherit">
-                    Have a project idea to collaborate with?
-                    <img src={orange} alt="orange" />
-                </FooterTitle>
+                <FooterTitle weight="bold" variant="h5" align="inherit">
+                    Have a project? Contact me to collaborate
+                </FooterTitle>{" "}
+                <ContactForm>
+                    <Input
+                        type="text"
+                        placeholder="First Name"
+                        shape="rounded"
+                        size="lg"
+                        value=""
+                        onChange={() => {}}
+                    />
+                    <Input
+                        type="text"
+                        placeholder="Last Name"
+                        shape="rounded"
+                        size="lg"
+                        value=""
+                        onChange={() => {}}
+                    />
+                    <Input
+                        type="tel"
+                        placeholder="Phone number"
+                        shape="rounded"
+                        size="lg"
+                        value=""
+                        onChange={() => {}}
+                    />
+                    <Input
+                        type="email"
+                        placeholder="Email"
+                        shape="rounded"
+                        size="lg"
+                        value=""
+                        onChange={() => {}}
+                    />
 
-                <a href="mailto:sharipovah01@gmail.com">
-                    <Button
-                        size="sm"
-                        color="success"
-                        shape="circle"
-                        variant="outlined"
-                    >
-                        Contact me
-                    </Button>
-                </a>
+                    <StyledTextarea
+                        type="textarea"
+                        placeholder="Message or description"
+                        shape="rounded"
+                        size="lg"
+                        value=""
+                        onChange={() => {}}
+                    />
+
+                    <a href="mailto:sharipovah01@gmail.com">
+                        <Button
+                            size="sm"
+                            color="success"
+                            shape="circle"
+                            variant="outlined"
+                        >
+                            Contact me
+                        </Button>
+                    </a>
+                </ContactForm>
             </FooterWrapper>
             <SocialMedia>
                 <Icon iconName="instagram" />
