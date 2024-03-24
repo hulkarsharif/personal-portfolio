a2e01d68aba22b67d1e33410c1b0a41029e33dce
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-03-24 11:06:09 -0400

    Update the resume

--- src/pages/Navigation/Top-navigation.tsx
+++ src/pages/Navigation/Top-navigation.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { NavigationLink } from "./Links";
-// import logoMe from "../../design-system/assets/image/logo11.png";
+import logoMe from "../../design-system/assets/image/Klogo.png";
 import { PageContainer } from "../components";
 
 const links = [
